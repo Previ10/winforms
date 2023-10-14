9bd8094323266fa471bc785d4eb03443540f38bd
Ignacio Previgliano <115052879+Previ10@users.noreply.github.com>
2023-10-14 20:46:57 -0300

    se agregan archivos de winforms

---
+++ winformsProfes/winformsProfes/Persona.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Comision_B
+{
+    abstract class Persona { 
+    
+    
+        private int id;
+        private string nombre;
+        private string apellido;
+        private string dni;
+        private string cuil;
+
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+
+        public string Apellido
+        {
+            get { return apellido; }
+            set { apellido = value; }
+        }
+
+        public string DNI
+        {
+            get { return dni; }
+            set { dni = value; }
+        }
+
+        public string Cuil
+        {
+            get { return cuil; }
+            set { cuil = value; }
+        }
+
+
+        public Persona(int id, string nombre, string apellido, string dni, string cuil)
+        {
+            this.id = id;
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.dni = dni;
+            this.cuil = cuil;
+        }
+
+
+        public Persona()
+        {
+
+        }
+
+      
+
+
+    }
+}
---
+++ winformsProfes/winformsProfes/Profesor.cs
@@ -0,0 +1,61 @@
+﻿using Proyecto_Comision_B;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Proyecto_Comision_B
+{
+    internal class Profesor : Empleado, Isueldo
+    {
+        public string Materia { get; set; }
+        public int Antiguedad { get; set; }
+        public string ART { get; set; }
+
+        public Profesor(string cargo, int legajo, string art, string obraSocial, float sueldoBruto, string materia, int antiguedad, int id, string nombre, string apellido, string dni, string cuil)
+     : base(cargo, legajo, art, obraSocial, sueldoBruto, id, nombre, apellido, dni, cuil)
+        {
+            Materia = materia;
+            Antiguedad = antiguedad;
+        }
+
+        public new float SueldoBruto
+        {
+            get { return base.SueldoBruto; }
+            set { base.SueldoBruto = value; }
+        }
+        public float CalcularSueldoNeto()
+        {
+            
+            float obraSocial = SueldoBruto * 0.03f;
+            float descuentoINSSJP = SueldoBruto * 0.03f;
+            float jubilacion = SueldoBruto * 0.11f;
+            float deduccionTotal = obraSocial + descuentoINSSJP + jubilacion;
+            float sueldoNeto = SueldoBruto - deduccionTotal;
+            return sueldoNeto;
+        }
+
+        public void Evaluar()
+        {
+            
+        }
+
+        public string Calificar()
+        {
+            return "Estoy calificando";
+        }
+
+        public string ObtenerMateria()
+        {
+            return Materia;
+        }
+
+        public int ObtenerAntiguedad()
+        {
+            return Antiguedad;
+        }
+    }
+}
